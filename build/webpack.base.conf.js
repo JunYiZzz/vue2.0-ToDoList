9e4fe5af99a1540e32b9774eb799372b84a6dac2
Unknown <18968151786@163.com>
2018-06-08 16:50:07 +0800

    功能添加

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,6 +75,16 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
+      ,
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader",
+      }
+      ,
+      {
+        test:/\.css$/,
+        loader:'css-loader!style-loader',
+      }
     ]
   },
   node: {
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="todo">
-    <h2>ToDoList</h2>
+    <div class="bg_title">
+      <h2 v-text="title1"></h2>
+    </div>
     <div class="t_input">
-      <span><input type="text" class="t_titles" placeholder="请输入事项。。。" v-model="text" @keyup.13="add()" ></span>
+      <span><input type="text" class="t_titles" placeholder="请输入数据。。。" v-model="text" @keyup.13="add()" ></span>
       <span><button class="t_add" @click="add()">添加</button></span>
     </div>
     <div class="t-cont">
@@ -12,10 +14,19 @@
           {{`${index+1}. `}} {{item.value}}
           <span @click="del(index)">删除</span>
           <span @click="edit(index)">编辑</span>
+          <span @click="aldone(index)">完成</span>
         </li>
-        <div class="check_che" @click="remove()" v-show="select.length > 0">删除选中</div>
+        <div class="check_che"  v-show="select.length > 0">已选择{{select.length}}项 <span @click="remove()">删除选中</span></div>
         <p v-show="todolist.length == 0">暂无数据！</p>
-
+        <div class="bg_title">
+          <h2 v-text="title2"></h2>
+        </div>
+        <li v-for="(item,index) in alreadylist" track-by="$index" :key="item.id">
+          {{`${index+1}. `}} {{item.value}}
+          <span @click="deldone(index)">删除</span>
+          <span @click="havetodo(index)">未完成</span>
+        </li>
+        <p v-show="alreadylist.length == 0">暂无数据！</p>
       </ul>
     </div>
   </div>
@@ -28,35 +39,55 @@ export default {
     return {
       todolist: [],
       text: '',
-      state: false
+      state: false,
+      alreadylist: [],
+      title1: 'ToDoList',
+      title2: 'AlreadyDoneList'
     }
   },
   methods: {
-    // 添加
+    // 添加未完成
     add () {
       if (this.text === '') {
         return
       } else {
         this.todolist.push({
           value: this.text,
-          state: false,
-          beforetext: this.text
+          state: false
         })
       }
       this.text = ''
     },
-    // 删除
+    // 未完成列表删除
     del (index) {
       this.todolist.splice(index, 1)
     },
-    // 删除选中
+    // 未完成列表删除选中
     remove () {
       this.todolist = this.todolist.filter((index) => !index.state)
     },
-    // 编辑
+    // 未完成列表编辑
     edit (index) {
       this.text = this.todolist[index].value
       this.todolist.splice(index, 1)
+    },
+    // 未完成列表已完成
+    aldone (index) {
+      this.alreadylist.push({
+        value: this.todolist[index].value
+      })
+      this.todolist.splice(index, 1)
+    },
+    // 已完成列表删除
+    deldone (index) {
+      this.alreadylist.splice(index, 1)
+    },
+    // 已完成列表未完成
+    havetodo (index) {
+      this.todolist.push({
+        value: this.alreadylist[index].value
+      })
+      this.alreadylist.splice(index, 1)
     }
   },
 
@@ -69,85 +100,85 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+  @bordercolor:#ccc;
+  @checkcolor:deepskyblue;
+  .bg_title,.t_input{
+    text-align: center;
+  }
   .t_titles {
     width: 400px;
     height: 35px;
     font-size: 16px;
     padding-left: 10px;
   }
-
   .t_add {
     width: 60px;
     height: 40px;
     font-size: 16px;
     vertical-align: middle;
   }
-
   .t-cont {
     width: 100%;
     margin-top: 20px;
     margin: 0 auto;
+    ul li{
+      list-style: none;
+      height: 30px;
+      border: 1px solid @bordercolor;
+      line-height: 30px;
+      text-align: left;
+      padding-left: 10px;
+    span{
+      float: right;
+      cursor: pointer;
+      padding-right: 10px;
+    }
+    input{
+      padding-top: 5px;
+    }
   }
-
-  .t-cont ul li {
-    list-style: none;
-    height: 30px;
-    border: 1px solid #ccc;
-    line-height: 30px;
-    text-align: left;
-    padding-left: 10px;
+    ul p{
+      text-align: center;
+    }
   }
-
-  .t-cont ul li span {
-    float: right;
-    cursor: pointer;
-    padding-right: 10px;
-  }
-
-  .t-cont ul li input {
-    padding-top: 5px;
-  }
-
   .checks {
     width: 15px;
     height: 15px;
     border-radius: 50%;
-    border: 1px solid #ccc;
+    border: 1px solid @bordercolor;
     display: inline-block;
     margin-top: 5px;
     cursor: pointer;
   }
-
-  .checkbox {
+  .checkbox{
     width: 18px;
     height: 18px;
-    border: 1px solid #ccc;
+    border: 1px solid @bordercolor;
     border-radius: 100%;
     position: relative;
     margin-right: 6px;
     vertical-align: middle;
+    &:checked{
+      border-color: @checkcolor;
+      &:before{
+        content: '';
+        display: block;
+        width: 80%;
+        height: 80%;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        background: @checkcolor;
+        border-radius: 100%;}
+    }
   }
-
-  .checkbox:checked {
-    border-color: deepskyblue;
-  }
-
-  .checkbox:checked:before {
-    content: '';
-    display: block;
-    width: 80%;
-    height: 80%;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    background: deepskyblue;
-    border-radius: 100%;
-  }
-
   .check_che {
-    border: 1px solid #ccc;
-    padding: 10px;
+    border: 1px solid @bordercolor;
+    padding:10px;
+    span{
+      float: right;
+    }
   }
 </style>
